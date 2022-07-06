267b6f5d33298c5b8e43d70f71a00cefea865385
Carter Sprigings <carter@blacklithium.com>
2022-07-06 11:06:21 -0400

    feat: add retained backups

---
+++ backup.tf
@@ -0,0 +1,19 @@
+module "backup" {
+  source                         = "cloudposse/backup/aws"
+  version                        = "0.12.0"
+  stage                          = terraform.workspace
+  name                           = var.environment[terraform.workspace]
+  backup_resources               = [aws_db_instance.postgres.arn]
+  not_resources                  = []
+  schedule                       = "cron(0 5 * * ? *)"
+  start_window                   = 120
+  completion_window              = 720
+  delete_after                   = 30
+}
+
+resource "aws_backup_vault_lock_configuration" "backup_lock" {
+  backup_vault_name   = module.backup.backup_vault_id
+  changeable_for_days = 3
+  max_retention_days  = 365
+  min_retention_days  = 7
+}
