5661314a05ecdeacd6b3a0db42899e874bdee9c9
Carter Sprigings <carter@blacklithium.com>
2022-07-19 09:59:17 -0400

    fix: pin to `4.3.6` for api and frontend

--- variables.tf
+++ variables.tf
@@ -165,7 +165,7 @@ variable "services" {
 
   default = [
     {
-      tag         = "4"
+      tag         = "4.3.6"
       name        = "api"
       mem         = 6144
       cpu         = 1536
@@ -194,7 +194,7 @@ variable "services" {
       ulimits     = null
     },
     {
-      tag         = "4"
+      tag         = "4.3.6"
       name        = "frontend"
       mem         = 1024
       cpu         = 256
@@ -214,7 +214,7 @@ variable "services" {
       ulimits     = null
     },
     {
-      tag       = "4"
+      tag       = "4.0.0"
       name      = "auth"
       mem       = 1024
       cpu       = 256
