b4abdd58492590df8ab8894751401ae76026ea80
andrew.mcewen96@gmail.com <andrew.mcewen96@gmail.com>
2022-08-17 07:48:21 -0400

    chore(version): bump to 4.6.3

--- variables.tf
+++ variables.tf
@@ -165,7 +165,7 @@ variable "services" {
 
   default = [
     {
-      tag         = "4.6.2"
+      tag         = "4.6.3"
       name        = "api"
       mem         = 6144
       cpu         = 1536
@@ -194,7 +194,7 @@ variable "services" {
       ulimits     = null
     },
     {
-      tag         = "4.6.2"
+      tag         = "4.6.3"
       name        = "frontend"
       mem         = 1024
       cpu         = 256
