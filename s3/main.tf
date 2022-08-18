e2512a99d938a096a8408898cdff91d5e48e1772
Carter Sprigings <carter@blacklithium.com>
2022-08-18 12:16:32 -0400

    feat: allow cors access to bucket; req'd for latest version

--- s3/main.tf
+++ s3/main.tf
@@ -1,7 +1,13 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.name}-${var.environment}"
   acl    = var.acl
-
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
