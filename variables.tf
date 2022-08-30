da55e597eb3f21b3c0d78793028289bc65721241
andrew.mcewen96@gmail.com <andrew.mcewen96@gmail.com>
2022-08-30 17:58:14 -0400

    chore(version): bump to 5.1.1

--- variables.tf
+++ variables.tf
@@ -165,7 +165,7 @@ variable "services" {
 
   default = [
     {
-      tag         = "5.1.0"
+      tag         = "5.1.1"
       name        = "api"
       mem         = 6144
       cpu         = 1536
@@ -194,7 +194,7 @@ variable "services" {
       ulimits     = null
     },
     {
-      tag         = "5.1.0"
+      tag         = "5.1.1"
       name        = "frontend"
       mem         = 1024
       cpu         = 256
