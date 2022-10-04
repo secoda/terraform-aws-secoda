a274bc8e99b01e6f43b9fa541cac6eb773b8a8a3
Carter Sprigings <carter@blacklithium.com>
2022-10-04 12:00:26 -0400

    feat: add vpc outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "security_group_id" {
   description = "ECS security group ID used for Secoda"
   value       = aws_security_group.ecs_sg.id
 }
+
+output "vpc_id" {
+  description = "VPC ID used for Secoda"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Subnet IDs used for Secoda"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "Subnet IDs used for Secoda"
+  value       = module.vpc.private_subnet_arns
+}
