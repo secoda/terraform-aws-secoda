10d31f475fd39eb11eedbe0265c02c4c724fd75a
Carter Sprigings <carter@blacklithium.com>
2022-10-26 16:07:02 -0400

    chore(version): bump to 5.4.0; includes databuilder v2; new optional auth

--- variables.tf
+++ variables.tf
@@ -165,7 +165,7 @@ variable "services" {
 
   default = [
     {
-      tag         = "5.2.2"
+      tag         = "5.4.0"
       name        = "api"
       mem         = 6144
       cpu         = 1536
@@ -194,7 +194,7 @@ variable "services" {
       ulimits     = null
     },
     {
-      tag         = "5.2.2"
+      tag         = "5.4.0"
       name        = "frontend"
       mem         = 1024
       cpu         = 256
