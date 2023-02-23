bf0e8e5fb5e954e913f565045f81c1d9154834b0
andrewmcewen <andrew@secoda.co>
2023-02-23 18:39:12 -0500

    chore(version): bump to v6.1.9

--- variables.tf
+++ variables.tf
@@ -197,7 +197,7 @@ variable "services" {
 
   default = [
     {
-      tag       = "6.1.8"
+      tag       = "6.1.9"
       name      = "api"
       mem       = 6144
       cpu       = 1536
@@ -230,7 +230,7 @@ variable "services" {
       ulimits     = null
     },
     {
-      tag       = "6.1.8"
+      tag       = "6.1.9"
       name      = "frontend"
       mem       = 1024
       cpu       = 256
