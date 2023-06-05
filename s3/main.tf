16087bdcb7ebd2c29eff200683c8874ecc035644
Carter Sprigings <carter@blacklithium.com>
2023-06-05 14:54:08 -0400

    chore(bump): upgrade modules

--- s3/main.tf
+++ s3/main.tf
@@ -8,11 +8,15 @@ resource "aws_s3_bucket" "bucket" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_enc" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.48.0"
+      version = ">= 5.1.0"
     }
   }
 
-  # backend "remote" {
-  #   # If using terraform cloud, please replace `organization = "secoda"` with your organization name.
-  #   organization = "secoda"
-  #   workspaces {
-  #     name = "secoda-on-premise-test"
-  #   }
-  # }
+  backend "remote" {
+    # If using terraform cloud, please replace `organization = "secoda"` with your organization name.
+    organization = "secoda"
+    workspaces {
+      name = "secoda-on-premise-test"
+    }
+  }
 }
