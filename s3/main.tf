c476754f380db38d8ef5da381ee71a6d6e68e7ee
Carter Sprigings <carter@blacklithium.com>
2023-08-16 10:09:47 -0400

    chore: Update AWS provider version to >= 4.67.0

--- s3/main.tf
+++ s3/main.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.name}-${var.environment}"
   acl    = var.acl
+}
+
+resource "aws_s3_bucket_cors_configuration" "bucket_cors" {
+  bucket = aws_s3_bucket.bucket.id
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST", "GET"]
@@ -8,7 +13,6 @@ resource "aws_s3_bucket" "bucket" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
-
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_enc" {
