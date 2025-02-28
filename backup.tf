0c2521a72b87fdddf60a94fae2ccda59872298d9
Carter Sprigings <carter@blacklithium.com>
2025-02-28 15:47:39 -0500

    fix: Update backup schedule in backup.tf

--- backup.tf
+++ backup.tf
@@ -1,24 +1,22 @@
 module "backup" {
   source           = "cloudposse/backup/aws"
-  version          = "~> 1.0"
+  version          = "1.0.2"
   stage            = var.backup_name == null ? terraform.workspace : var.backup_name
   name             = var.name
   backup_resources = [aws_db_instance.postgres.arn]
   not_resources    = []
+  backup_vault_lock_configuration = {
+    changeable_for_days = 7
+    max_retention_days  = 365
+    min_retention_days  = 30
+  }
   rules = [
     {
-      name              = "secoda-db-daily"
-      schedule          = "cron(0 5 * * ? *)"
-      start_window      = 120
-      completion_window = 720
-      delete_after      = 30
+      name = "secoda-db-daily"
+      schedule : "cron(0 5 ? * * *)"
+      start_window      = 320
+      completion_window = 10080
+      delete_after      = 35
     }
   ]
-}
-
-resource "aws_backup_vault_lock_configuration" "backup_lock" {
-  backup_vault_name   = module.backup.backup_vault_id
-  changeable_for_days = 3
-  max_retention_days  = 365
-  min_retention_days  = 7
 }
