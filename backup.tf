5e0806734b4d310b0521118d8497bc0274524f6e
Carter <carter@blacklithium.com>
2025-03-25 18:03:46 -0400

    fix: carter/fix-backups-2 (#32)
    * Revert "fix: carter/adjust-backup-lifecycle (#30)"
    
    This reverts commit 39cedb9cc4924822a740cd8c65fd27dd86b41ae0.
    
    * fix: Update backup module version to 1.0.2

--- backup.tf
+++ backup.tf
@@ -5,22 +5,20 @@ module "backup" {
   name             = var.name
   backup_resources = [aws_db_instance.postgres.arn]
   not_resources    = []
-
-  backup_vault_lock_configuration = {
-    changeable_for_days = 7
-    max_retention_days  = 60
-    min_retention_days  = 14
-  }
-
   rules = [
     {
-      name = "secoda-db-daily"
-      schedule : "cron(0 5 ? * * *)"
-      start_window      = 320
-      completion_window = 10080
-      lifecycle = {
-        delete_after = 30
-      }
+      name              = "secoda-db-daily"
+      schedule          = "cron(0 5 * * ? *)"
+      start_window      = 120
+      completion_window = 720
+      delete_after      = 30
     }
   ]
+}
+
+resource "aws_backup_vault_lock_configuration" "backup_lock" {
+  backup_vault_name   = module.backup.backup_vault_id
+  changeable_for_days = 3
+  max_retention_days  = 365
+  min_retention_days  = 7
 }
